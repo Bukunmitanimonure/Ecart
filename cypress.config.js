02e3b142c4a65f1e53efe51d5bb9d995d67452c8
Bukunmi Tanimonure <bukunmitanimonure@gmail.com>
2023-03-15 12:59:14 +0100

    remove unused plugins

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    specPattern: "**/*.{feature,features}",
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
+      await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor", browserify.default(config));
+
+      return config;
     },
   },
 });
